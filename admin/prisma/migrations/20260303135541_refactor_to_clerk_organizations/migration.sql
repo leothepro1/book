23cf166a883e813c57a50fd4151100d5e3ba0bee
leothepro1 <leo@lrstudio.se>
2026-03-03 21:21:20 +0000

    fix: migration clerkOrgId backfill for existing tenants

--- admin/prisma/migrations/20260303135541_refactor_to_clerk_organizations/migration.sql
+++ admin/prisma/migrations/20260303135541_refactor_to_clerk_organizations/migration.sql
@@ -1,33 +1,21 @@
-/*
-  Warnings:
-
-  - You are about to drop the `TenantMember` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-  - A unique constraint covering the columns `[clerkOrgId]` on the table `Tenant` will be added. If there are existing duplicate values, this will fail.
-  - Added the required column `clerkOrgId` to the `Tenant` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- DropForeignKey
 ALTER TABLE "TenantMember" DROP CONSTRAINT "TenantMember_tenantId_fkey";
-
 -- DropForeignKey
 ALTER TABLE "TenantMember" DROP CONSTRAINT "TenantMember_userId_fkey";
-
--- AlterTable
-ALTER TABLE "Tenant" ADD COLUMN     "clerkOrgId" TEXT NOT NULL,
-ADD COLUMN     "ownerClerkUserId" TEXT;
-
+-- AlterTable: add columns as nullable first
+ALTER TABLE "Tenant" ADD COLUMN "clerkOrgId" TEXT,
+ADD COLUMN "ownerClerkUserId" TEXT;
+-- Backfill existing rows with a placeholder
+UPDATE "Tenant" SET "clerkOrgId" = 'org_placeholder_' || id WHERE "clerkOrgId" IS NULL;
+-- Now make it NOT NULL
+ALTER TABLE "Tenant" ALTER COLUMN "clerkOrgId" SET NOT NULL;
 -- DropTable
 DROP TABLE "TenantMember";
-
 -- DropTable
 DROP TABLE "User";
-
 -- DropEnum
 DROP TYPE "TenantRole";
-
 -- CreateIndex
 CREATE UNIQUE INDEX "Tenant_clerkOrgId_key" ON "Tenant"("clerkOrgId");
-
 -- CreateIndex
 CREATE INDEX "Tenant_clerkOrgId_idx" ON "Tenant"("clerkOrgId");
